fbbb1ae7013987d8435494a4431992d6ff9acd1b
James Tauber <jtauber@jtauber.com>
2013-08-18 02:18:50 -0400

    tweaks to mode function

--- modes.py
+++ modes.py
@@ -1,6 +1,6 @@
-def mode(mode_number):
-    X = [0, 2, 4, 6, 1, 3, 5]
-    mode = [([-7] * mode_number + [0] * (7 - mode_number))[6 - j] for j in X]
+def mode(n):
+    X = [i % 7 for i in range(0, 13, 2)]
+    mode = [([-7] * n + [0] * (7 - n))[6 - j] for j in X]
     return [a + b for a, b in zip(mode, X)]
 
 
